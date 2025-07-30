70ec19e166103250d7708902f9a0aad5ddf0aab5
usman <usmannaveed012@gmail.com>
2025-07-29 20:45:10 -0400

    updated the help commands to add roulette

--- src/commands/help.js
+++ src/commands/help.js
@@ -10,6 +10,7 @@ module.exports = {
         !gptchat <message> - Chat with the bot using GPT-4.
         !gptimage <image description> - Ask GPT-4 to generate an image for you
         !channelpoints - Displays how many channel points you have and how many messages you have sent in the discord
+        !roulette <amount to bet>- Gamble your channel points and play roulette with the bot
         `;
         message.channel.send(helpMessage);
     }
